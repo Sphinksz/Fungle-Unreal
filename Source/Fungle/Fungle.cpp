aeec0bd4f4f8c75d76b67a77ae60c6158438937a
Sphinksz <phoenix.jediorderonline@gmail.com>
2025-04-08 18:50:46 -0400

    [Added] Initial Unreal Base Project

---
+++ Source/Fungle/Fungle.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Fungle.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Fungle, "Fungle" );
+
+DEFINE_LOG_CATEGORY(LogFungle)
+ 
---
+++ Source/Fungle/FungleGameMode.cpp
@@ -0,0 +1,26 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "FungleGameMode.h"
+#include "FunglePlayerController.h"
+#include "FungleCharacter.h"
+#include "UObject/ConstructorHelpers.h"
+
+AFungleGameMode::AFungleGameMode()
+{
+	// use our custom PlayerController class
+	PlayerControllerClass = AFunglePlayerController::StaticClass();
+
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/TopDown/Blueprints/BP_TopDownCharacter"));
+	if (PlayerPawnBPClass.Class != nullptr)
+	{
+		DefaultPawnClass = PlayerPawnBPClass.Class;
+	}
+
+	// set default controller to our Blueprinted controller
+	static ConstructorHelpers::FClassFinder<APlayerController> PlayerControllerBPClass(TEXT("/Game/TopDown/Blueprints/BP_TopDownPlayerController"));
+	if(PlayerControllerBPClass.Class != NULL)
+	{
+		PlayerControllerClass = PlayerControllerBPClass.Class;
+	}
+}
