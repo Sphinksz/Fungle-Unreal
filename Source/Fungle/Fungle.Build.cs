3bee59a645cb5c101241d7bbe848038b2ec56666
Sphinksz <phoenix.jediorderonline@gmail.com>
2025-04-25 21:29:01 -0400

    [Added] Main Menu Complete

--- Source/Fungle/Fungle.Build.cs
+++ Source/Fungle/Fungle.Build.cs
@@ -8,6 +8,16 @@ public class Fungle : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-        PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "NavigationSystem", "AIModule", "Niagara", "EnhancedInput" });
+        PublicDependencyModuleNames.AddRange(new string[]
+        {
+	        "Core", 
+	        "CoreUObject", 
+	        "Engine", 
+	        "InputCore", 
+	        "NavigationSystem", 
+	        "AIModule", 
+	        "Niagara", 
+	        "EnhancedInput"
+        });
     }
 }
